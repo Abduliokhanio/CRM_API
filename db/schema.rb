cecd29ef50ebe2efd9c118dc3b8fbdcc22c972cf
Abdul Khan <akhan17@outlook.com>
2021-03-08 15:28:33 -0600

    --

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_203358) do
-
-  create_table "leads", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_leads_on_user_id"
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.text "description"
-    t.integer "user_id"
-    t.integer "lead_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
